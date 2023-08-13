eb5394b12d568971cc7859b5a4a428390bbcadd9
pradnya deshmukh <101960678+Pradnya1903@users.noreply.github.com>
2023-08-13 18:20:03 +0530

    Create IndexFirstOccuringString.java
    Solved by Two Pointers , String , String matching

---
+++ IndexFirstOccuringString.java
@@ -0,0 +1,15 @@
+/*
+
+Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+*/
+
+class Solution {
+    public int strStr(String haystack, String needle) {
+        
+        if(haystack.contains(needle))
+        {
+            return haystack.indexOf(needle);
+        }
+     return -1 ;
+    }
+}
